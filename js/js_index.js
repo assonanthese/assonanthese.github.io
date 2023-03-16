f63e51e1565d9cb340e10238463482e7f03800b3
lucas91380 <lucas.chabeau@gmail.com>
2023-03-16 15:19:17 +0100

    Add site out of docs

---
+++ js/js_index.js
@@ -0,0 +1,11 @@
+alert("Bonjour");
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
